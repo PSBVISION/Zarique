990d56c4376f115c513ce91d8c318dda7f1bf1d7
psbvision <punitsinghbisht1529@gmail.com>
2025-06-24 22:31:09 +0530

    fixes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             className="w-5 cursor-pointer"
           />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className=" flex flex-col gap-2 w-42 py-3 px-5 bg-slate-100 text-gray-500 text-xl rounded">
+            <div className=" flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Orders</p>
               <p className="cursor-pointer hover:text-black">Log Out</p>
@@ -48,15 +48,15 @@ const Navbar = () => {
           </div>
         </div>
         <Link to="/cart" className="relative">
-          <img src={assets.cart_icon} alt="cart" className="w-7 min-w-5" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-5 text-center leading-5 bg-black text-white aspect-square rounded-full text-[14px]">
+          <img src={assets.cart_icon} alt="cart" className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             10
           </p>
         </Link>
         <img
           onClick={() => setVisible(true)}
           src={assets.menu_icon}
-          className="w-7 cursor-pointer sm:hidden"
+          className="w-5 cursor-pointer sm:hidden"
           alt=""
         />
       </div>
